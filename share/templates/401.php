face3dca87a2f2a00464707fb03b4e3453b983cd
Dejan Spasic <dejan.spasic@trivago.com>
2015-07-28 01:40:50 +0200

    Use the function http_response_code to send the header

--- share/templates/401.php
+++ share/templates/401.php
@@ -13,10 +13,11 @@ ob_start();
     The credentials provided for the Web source, are invalid.
 </p>
 <?php
-$view = new NoAuthView();
 $content = ob_get_clean();
 
 header('WWW-Authenticate: Basic realm="Pocs"');
-header('HTTP/1.0 401 Unauthorized');
+http_response_code(401);
+
+$view = new NoAuthView();
 
 include __DIR__ . '/layout.php';
