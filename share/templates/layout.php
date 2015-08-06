20f6868812e99a1dbcd409c4e30eb2f0ae0f84ee
Dejan Spasic <dejan.spasic@trivago.com>
2015-08-07 00:13:17 +0200

    Add explicit the meta charset tag

--- share/templates/layout.php
+++ share/templates/layout.php
@@ -2,6 +2,7 @@
 <meta lang="en" charset="utf-8">
 <html>
 <head>
+    <meta charset="utf-8">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <style>
         body {
